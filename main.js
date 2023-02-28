daf8feaa934a6207712de081d35797487f60c6ce
takamuu <mr.takamuu@gmail.com>
2023-02-28 14:56:41 +0900

    Clear KagamiMOchi

--- main.js
+++ main.js
@@ -9,9 +9,34 @@ function Main(input) {
   var c = parseInt(tmp[1], 10);
   var s = input[2];
   //出力
+  console.log('---atcoder---');
   console.log('%d %s', a + b + c, s);
 }
 
 Main(`1
 2 3
 test`);
+
+//============================================================================
+console.log('');
+console.log('---ABC085B - Kagami Mochi---');
+const KagamiMochi = (input) => {
+  const diameter = input.split('\n');
+  const N = parseInt(diameter[0]);
+
+  const numDiameter = diameter.slice(1, N + 1).map((str) => parseInt(str, 10));
+
+  // const answer = new Set(numDiameter).size;
+  // console.log(answer);
+  const tiers = Array.from(new Set(numDiameter));
+  console.log(tiers.length);
+};
+
+KagamiMochi(`7
+50
+30
+50
+100
+50
+80
+30`);
